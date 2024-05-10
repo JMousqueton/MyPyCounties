9342c2dc2d494756cd6497f752a7fd8fc0b4a35b
Julien Mousqueton <julien@mousqueton.io>
2024-05-10 08:52:21 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mypycountries',
+    version='0.1',
+    packages=find_packages(),
+    description='Simple library to manipulate countries',
+    long_description=open('README.md').read(),
+    author='Julien Mousqueton',
+    author_email='julien+github@mousqueton.io',
+    url='https://www.ransomware.live',
+)
